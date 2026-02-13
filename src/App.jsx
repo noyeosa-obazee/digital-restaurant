891d6582104fbef76dd217734943fa4236704a79
noyeosa-obazee <obazeenoyeosa@gmail.com>
2026-02-13 14:26:35 +0100

    chore: set up vite react and define project folder structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Digital Restaurant</h1>
+      <p>Frontend boilerplate ready.</p>
+    </div>
+  );
+}
+
+export default App;
