8dfccae37e2ae357138db51aca07a9b55e17cd3d
noyeosa-obazee <obazeenoyeosa@gmail.com>
2026-02-16 14:39:52 +0100

    chore: replace login link ins signup page with react router link

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import familyMeal from "../../assets/images/family_meal.png";
 import Footer from "../Footer/Footer";
 import "./Signup.css";
@@ -262,7 +263,7 @@ const Signup = () => {
 
             <div className="form-footer">
               <p>
-                Already have an account? <a href="#">Sign In</a>
+                Already have an account? <Link to="/login">Sign In</Link>
               </p>
             </div>
           </form>
