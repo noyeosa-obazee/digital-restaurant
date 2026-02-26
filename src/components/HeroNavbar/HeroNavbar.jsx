907644614e081c5d062a55d30890d0bccfec46c5
noyeosa-obazee <obazeenoyeosa@gmail.com>
2026-02-26 11:24:01 +0100

    style: remove nav actions class from sign in btn in landing page

--- src/components/HeroNavbar/HeroNavbar.jsx
+++ src/components/HeroNavbar/HeroNavbar.jsx
@@ -2,19 +2,19 @@ import "./HeroNavbar.css";
 import { Link } from "react-router-dom";
 
 const HeroNavbar = () => {
-    return (
-        <nav className="navbar">
-            <div className="navbar-logo">
-                <span className="logo-text">Chuks Kitchen</span>
-            </div>
-            <div className="navbar-actions">
-                <Link to="/login">
-                    {" "}
-                    <button className="sign-in-btn">Sign In</button>
-                </Link>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <span className="logo-text">Chuks Kitchen</span>
+      </div>
+      <div className="">
+        <Link to="/login">
+          {" "}
+          <button className="sign-in-btn">Sign In</button>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default HeroNavbar;
